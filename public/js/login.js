2980a3e58f76d404a19b59790565abb5e0440293
M1rzebeyM1rzebeyli <mirzemurad88888@gmai.com>
2024-07-17 16:51:00 +0400

    login done

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+document.getElementById("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const keys = ["username", "password"]
+
+    keys.forEach(key => {
+        document.getElementById(`${key}error`).innerText = "";
+    });
+
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+
+    let res = await fetch("/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: formData.get("username"),
+            password: formData.get("password"),
+        })
+    });
+
+    res = await res.json()
+
+    if (res.success) {
+        window.location.href = "/";
+    } else {
+        Object.keys(res.errors).forEach(key => {
+            document.getElementById(`${key}error`).innerText = res.errors[key]
+        });
+    }
+});
